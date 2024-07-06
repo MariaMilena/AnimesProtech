6a78bb418dc713e5a27b9d6a6f519bd1d7c1ef64
Maria Milena <milena20182018@gmail.com>
2024-07-06 01:30:56 -0300

    feat: Implementação dos testes Unitários

---
+++ Crud.test/Animes/Commands/DeleteAnimeCommandTests.cs
@@ -0,0 +1,30 @@
+﻿
+using Crud.Application.Animes.Commands;
+using Crud.Domain.Entities;
+using Crud.Domain.Interfaces;
+using Moq;
+using static Crud.Application.Animes.Commands.DeleteAnimeCommand;
+
+namespace Crud.UnitTests.Animes.Commands;
+
+public class DeleteAnimeCommandTests
+{
+    [Fact]
+    public async Task DeleteCommandWithNonExistentId_Fail()
+    {
+        // Arrange
+        var unitOfWork = new Mock<IUnitOfWork>();
+
+        unitOfWork.Setup(u => u.AnimeRepository.GetAnimeById(It.IsAny<int>())).ReturnsAsync((Anime)null);
+
+        var deleteAnimeCommand = new DeleteAnimeCommand { Id = 1 };
+
+        var deleteAnimeCommandHandler = new DeleteAnimeCommandHandler(unitOfWork.Object);
+
+        // Act
+        Func<Task> act = async () => await deleteAnimeCommandHandler.Handle(deleteAnimeCommand, new CancellationToken());
+
+        // Assert
+        await Assert.ThrowsAsync<ArgumentException>(act);
+    }
+}
---
+++ Crud.test/Animes/Queries/GetAnimeByIdQueryTests.cs
@@ -0,0 +1,50 @@
+﻿
+using Crud.Application.Animes.Queries;
+using Crud.Domain.Entities;
+using Crud.Domain.Interfaces;
+using Moq;
+using static Crud.Application.Animes.Queries.GetAnimeByIdQuery;
+
+namespace Crud.UnitTests.Animes.Queries;
+
+public class GetAnimeByIdQueryTests
+{
+    [Fact]
+    public async Task QueryWithValidId_Executed_Success()
+    {
+        // Arrange
+        var unitOfWork = new Mock<IUnitOfWork>();
+
+        var existingAnime = new Anime("Nome original", "Resumo original", "Diretor original");
+        unitOfWork.Setup(u => u.AnimeRepository.GetAnimeById(It.IsAny<int>())).ReturnsAsync(existingAnime);
+
+        var getAnimeByIdQuery = new GetAnimeByIdQuery { Id = 1 };
+
+        var getAnimeByIdQueryHandler = new GetAnimeByIdQueryHandler(unitOfWork.Object);
+
+        // Act
+        var result = await getAnimeByIdQueryHandler.Handle(getAnimeByIdQuery, new CancellationToken());
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(existingAnime.Name, result.Name);
+        Assert.Equal(existingAnime.Summary, result.Summary);
+        Assert.Equal(existingAnime.Director, result.Director);
+    }
+
+    [Fact]
+    public async Task QueryWithNonExistentId_Fail()
+    {
+        // Arrange
+        var unitOfWork = new Mock<IUnitOfWork>();
+
+        unitOfWork.Setup(u => u.AnimeRepository.GetAnimeById(It.IsAny<int>())).ReturnsAsync((Anime)null);
+
+        var getAnimeByIdQuery = new GetAnimeByIdQuery { Id = 1 };
+
+        var getAnimeByIdQueryHandler = new GetAnimeByIdQueryHandler(unitOfWork.Object);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ArgumentException>(() => getAnimeByIdQueryHandler.Handle(getAnimeByIdQuery, new CancellationToken()));
+    }
+}
